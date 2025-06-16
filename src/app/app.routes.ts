f028f83339b3ad01d4328fc3bc70bc96f647d6c5
marco-soria <marco.soriaa7@gmail.com>
2025-06-16 16:03:40 -0500

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    //loadComponent: () => import('./gifs/pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    loadComponent: () => import('./gifs/pages/dashboard/dashboard.component'), // Assuming the component is exported as default
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+      import('./gifs/pages/trending/trending.component').then(
+        (m) => m.TrendingComponent
+      ),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./gifs/pages/search/search.component').then(
+        (m) => m.SearchComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
