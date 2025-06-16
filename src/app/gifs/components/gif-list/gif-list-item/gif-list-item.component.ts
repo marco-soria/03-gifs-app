d9317b0ed41c0cd44d5a5e627ea23f80f0365735
marco-soria <marco.soriaa7@gmail.com>
2025-06-16 15:22:45 -0500

    initial c reation of files

---
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list-item',
+  imports: [],
+  templateUrl: './gif-list-item.component.html',
+  styleUrl: './gif-list-item.component.css'
+})
+export class GifListItemComponent {
+
+}
---
+++ src/app/gifs/components/gif-list/gif-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list',
+  imports: [],
+  templateUrl: './gif-list.component.html',
+  styleUrl: './gif-list.component.css'
+})
+export class GifListComponent {
+
+}
